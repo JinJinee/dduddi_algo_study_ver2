0c386d877303a44f71584faf55e075b3ffc69fb6
juhyemi <114149911+juhyemi@users.noreply.github.com>
2022-11-07 12:26:41 +0900

    [Solved✌🏻]수진_아스키코드

---
+++ 2022년11월07일/q11654.java
@@ -0,0 +1,16 @@
+package Bjalgorism;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q11654 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int n = s.charAt(0);
+		
+		System.out.println(n);
+	}
+
+}
